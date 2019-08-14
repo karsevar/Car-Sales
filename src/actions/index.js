4af5dbffe962a4b27fd389131c50990dd4f7a125
masonkarsevar <masonkarsevar@gmail.com>
2019-08-14 08:14:25 -0700

    mock action-creator written

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+export const ADD_FEATURE = 'ADD_FEATURE';
+
+export const addFeature = feature => {
+    console.log(feature);
+    return {
+        type: ADD_FEATURE,
+        payload: feature
+    }
+}
+
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+
+export const removeFeature = feature => {
+    console.log(feature);
+    return {
+        type: REMOVE_FEATURE
+    }
+}
